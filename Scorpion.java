4992f22539e0383e4d6674ca37150783540ee2ef
Kanchana Ranasinghe <kahnchana@gmail.com>
2017-10-28 18:09:57 +0530

    .

--- Scorpion.java
+++ Scorpion.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 
 
-public class Scorpion extends BravoBot{
+public class Scorpion2 extends CharlieBot{
 	Random randomNum=new Random();
 
 	public void run() {
@@ -20,59 +20,47 @@ public class Scorpion extends BravoBot{
 	//	double FWidth=getBattleFieldWidth();
 		double FHeight=getBattleFieldHeight();
 		// Loop forever
-		while (getOthers()>3) {
-			turnRight(40+randomNum.nextInt(20));
-			ahead(5000);
-		}
-		while (getOthers()<4) {
-			turnRight(50);
-			ahead(5000);
-			turnRight(60);
+		while (true) {
+			turnRight(10+randomNum.nextInt(10));
 			ahead(5000);
 		}
 	}
 
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-	   	if (e.getDistance() < 50 && getOthers()>6) {
-           fire(5);
-       } else if (e.getDistance() < 100 && getOthers()>4){
-           fire(4);
-       } else if (e.getDistance() < 200 && getOthers()>3){
-           fire(3);
-       } else if (e.getDistance() < 400){
+	public void onRobotDetected(ScannedRobotEvent e) {
+	   	if (e.getDistance() < 50 && getOthers()>5) {
            fire(2);
+       } else if (e.getDistance() < 100 && getOthers()>3){
+           fire(1);
+       } else if (e.getDistance() < 200 && getOthers()>3){
+           fire(1);
+       } else if (e.getDistance() < 400){
+           fire(1);
        } 
 	}
 
 
 	public void onHitByBullet(HitByBulletEvent e){
 		double BHead=e.getHeading();
-		if(getHeading()-BHead>-30 && getHeading()-BHead<30){
-			turnRight(60);
+		if(getHeading()-BHead>-25 && getHeading()-BHead<25){
+			turnRight(5);
 			System.out.println("hit");
 		}
 	}
 	
 
 	public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() > -20 && e.getBearing() < 20) {
-			fire(5);
-			System.out.println("ram");
+		if (e.getBearing() > -10 && e.getBearing() < 10) {
+			fire(2);
+			System.out.println("abc");
 		}
-		else if (e.getBearing() > 160 && e.getBearing() < -160) {
-			fire(5);
-			System.out.println("ram");
-		}
-		//ahead(10);
 	
 	}
 
 
 	public void onHitWall(HitWallEvent e){
 		double head=getHeading();
-		turnRight(10);
-		System.out.println("wall");
+		//turnRight(10);
 	}
 	
 }
