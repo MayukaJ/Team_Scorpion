0131c36396da0924c349667c5d7b9d5c690d1499
Kanchana Ranasinghe <kahnchana@gmail.com>
2017-10-28 15:33:08 +0530

    .

--- Scorpion.java
+++ Scorpion.java
@@ -1,10 +1,15 @@
+import java.util.Random;
+
 //import robocode.Robot;
 import robocode.*;
 //import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
+
+
 public class Scorpion extends AlphaBot{
+	Random randomNum=new Random();
 
 	public void run() {
 		// Set colors
@@ -15,57 +20,59 @@ public class Scorpion extends AlphaBot{
 	//	double FWidth=getBattleFieldWidth();
 		double FHeight=getBattleFieldHeight();
 		// Loop forever
-		while (true) {
-			// Tell the game that when we take move,
-			// we'll also want to turn right... a lot.
+		while (getOthers()>3) {
+			turnRight(80+randomNum.nextInt(100));
+			System.out.println(80+randomNum.nextInt(80));
+			ahead(5000);
+		}
+		while (getOthers()<4) {
+			turnRight(70);
+			ahead(5000);
 			turnRight(90);
-			ahead(500);
-			turnRight(100);
-			//ahead(400);
-			if (getY()==(FHeight/2)){
-				turnLeft(360);
-			}
-			// Limit our speed to 5
-			// Start moving (and turning)
-			ahead(1000);
-		
-			
-			// Repeat.
+			ahead(5000);
 		}
 	}
 
-	/**
-	 * onScannedRobot: Fire hard!
-	 */
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-	
-		if (e.getDistance() < 50) {
+	   	if (e.getDistance() < 50 && getOthers()>6) {
            fire(5);
-       } else if (e.getDistance() < 100){
+       } else if (e.getDistance() < 100 && getOthers()>4){
            fire(4);
-       } else if (e.getDistance() < 200){
+       } else if (e.getDistance() < 200 && getOthers()>3){
            fire(3);
        } else if (e.getDistance() < 400){
            fire(2);
        } 
 	}
 
-	/**
-	 * onHitRobot:  If it's our fault, we'll stop turning and moving,
-	 * so we need to turn again to keep spinning.
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() > -10 && e.getBearing() < 10) {
-			fire(3);
-		}
-		if (e.isMyFault()) {
-			turnRight(10);
+
+	public void onHitByBullet(HitByBulletEvent e){
+		double BHead=e.getHeading();
+		if(getHeading()-BHead>-30 && getHeading()-BHead<30){
+			turnRight(120);
+			System.out.println("hit");
 		}
 	}
 	
-	public void onHitWall(HitWallEvent e) {
-		double head=getHeading();
-		turnRight(120-(head%90));		
+
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() > -20 && e.getBearing() < 20) {
+			fire(5);
+			System.out.println("ram");
+		}
+		else if (e.getBearing() > 160 && e.getBearing() < -160) {
+			fire(5);
+			System.out.println("ram");
+		}
+		//ahead(10);
+	
 	}
-			
+
+
+	public void onHitWall(HitWallEvent e){
+		double head=getHeading();
+		turnRight(90-(head%90)+30);
+	}
+	
 }
